56f5ea39067c3944712b48507de0f72fd44e2401
Verbindolai <nikolaiwieczorek@web.de>
2020-12-10 16:40:44 +0100

    Initialised Structure and added first Commands

--- src/command.ts
+++ src/command.ts
@@ -2,7 +2,6 @@
 import discord, { Client, GuildMember, Message } from 'discord.js';
 
  export interface command {
-
     name : string;
     execute(msg : Message, args: string[] , client : Client) : void;
     validation(msg : Message) : boolean;
