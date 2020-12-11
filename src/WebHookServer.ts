a6152c8ec159e45bfc4fdbea63cdcbd22a83bc68
Verbindolai <nikolaiwieczorek@web.de>
2020-12-11 16:46:17 +0100

    Added Secret Validation

--- src/WebHookServer.ts
+++ src/WebHookServer.ts
@@ -1,12 +1,42 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
+import crypto from "crypto";
+import fs from "fs";
 
 const app = express();
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const secret = config.gitSecret;
+const sigHeaderName = 'X-Hub-Signature-256'
+
 app.use(bodyParser.json());
 
 app.post('/', function(request, response){
+    const payload = JSON.stringify(request.body);
+
+    if (!payload){
+        console.log("Body is empty.")
+        return;
+    }
+
+    // calculate the signature
+    const expectedSignature = "sha256=" +
+        crypto.createHmac("sha256", secret)
+            .update(JSON.stringify(request.body))
+            .digest("hex");
+
+    //Request-Header Signature
+    const signature = request.headers[sigHeaderName];
+
+    // compare the signature against the one in the request
+    if (signature !== expectedSignature) {
+        throw new Error("Invalid signature.");
+    }
+
     console.log(request.body);
+    console.log(request.headers)
+
 });
 
 app.listen(7021);
 console.log("Listening on 7021...")
+
