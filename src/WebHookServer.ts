d9c1c66a8abb7266b645d9a0de6765e53878237f
Verbindolai <nikolaiwieczorek@web.de>
2020-12-11 18:10:04 +0100

    Made WebHookServer Implementation Object oriented

--- src/WebHookServer.ts
+++ src/WebHookServer.ts
@@ -1,47 +1,66 @@
-import express from 'express';
+import express, {Express, Request, Response} from 'express';
 import * as bodyParser from 'body-parser';
 import crypto from "crypto";
 import fs from "fs";
 
+class WebHookServer {
+     app : Express;
+     secret : string;
+     sigHeaderName : string;
 
-const app = express();
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const secret : string = config.gitSecret;
-const sigHeaderName = 'x-hub-signature-256';
+     constructor() {
+         this.app = express();
+         this.sigHeaderName = 'x-hub-signature-256';
+         this.secret = JSON.parse(fs.readFileSync('config.json', 'utf8')).gitSecret;
+         this.init();
+     }
 
-app.use(bodyParser.json());
+     init (): void {
+        this.app.use(bodyParser.json());
+     }
 
-app.post('/' ,function(request, response){
+     start(): void{
+         this.app.post('/' , (request, response) =>{
 
-    const payload = JSON.stringify(request.body);
+             const payload = JSON.stringify(request.body);
 
-    if (!payload){
-        console.log("Body is empty.");
-        return;
-    }
+             if (!payload){
+                 console.log("Body is empty.");
+                 return;
+             }
 
-    // calculate the signature
-    const expectedSignature = "sha256=" +
-        crypto.createHmac("sha256", secret)
-            .update(JSON.stringify(request.body))
-            .digest("hex");
+             if (!this.validateSignature(request, response)){
+                 response.status(500).send({
+                     message: 'Invalid signature.'
+                 });
+                 throw new Error("Invalid signature.");
+             }
 
-    //Request-Header Signature
-    const signature = request.headers[sigHeaderName];
+             this.action(request, response);
+         });
+         this.app.listen(7021);
+         console.log("Listening on 7021...");
+     }
 
-    // compare the signature against the one in the request
-    if (signature !== expectedSignature) {
-        response.status(500).send({
-            message: 'Invalid signature.'
-        });
-        throw new Error("Invalid signature.");
-    }
+     validateSignature(request: Request, response : Response) : boolean {
+         // calculate the signature
+         const expectedSignature = "sha256=" +
+             crypto.createHmac("sha256", this.secret)
+                 .update(JSON.stringify(request.body))
+                 .digest("hex");
 
-    console.log(request.body)
-    console.log(request.headers)
+         //Request-Header Signature
+         const signature = request.headers[this.sigHeaderName];
 
-});
+         return signature === expectedSignature;
+     }
 
-app.listen(7021);
-console.log("Listening on 7021...");
+     action(request: Request, response : Response) : void {
+         console.log(request.body)
+         console.log(request.headers)
+     }
+
+}
+let server : WebHookServer = new WebHookServer();
+server.start();
 
