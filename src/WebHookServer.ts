fb8c9476a3c9d1c0635eec1e45ae07956956d4c4
Verbindolai <nikolaiwieczorek@web.de>
2020-12-11 16:28:25 +0100

    Test

--- src/WebHookServer.ts
+++ src/WebHookServer.ts
@@ -1,34 +1,12 @@
-import http from 'http';
-import crypto from 'crypto';
-import { exec } from 'child_process';
-import fs from "fs";
+import express from 'express';
+import * as bodyParser from 'body-parser';
 
+const app = express();
+app.use(bodyParser.json());
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const SECRET = config.gitSecret;
+app.post('/', function(request, response){
+    console.log(request.body);
+});
 
-http
-    .createServer((req, res) => {
-        console.log("Request retrieved.")
-        req.on('data', chunk => {
-            console.log("Incoming Message...")
-            const signature = `sha1=${crypto
-                .createHmac('sha1', SECRET)
-                .update(chunk)
-                .digest('hex')}`;
-
-            const isAllowed = req.headers['x-hub-signature'] === signature;
-
-            const body = JSON.parse(chunk);
-
-            const isMaster = body?.ref === 'refs/heads/master';
-
-            if (isAllowed && isMaster) {
-                console.log("Worked!")
-            }
-        });
-
-        res.end();
-    })
-    .listen(7021);
-console.log("Listening on Port 7021...")
+app.listen(7021);
+console.log("Listening on 7021...")
