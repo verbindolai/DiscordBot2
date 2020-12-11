3f90b309ab9b202a19dbbaec182a57000d1e147f
Verbindolai <nikolaiwieczorek@web.de>
2020-12-11 17:20:37 +0100

    Test

--- src/WebHookServer.ts
+++ src/WebHookServer.ts
@@ -7,7 +7,7 @@ import fs from "fs";
 const app = express();
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const secret = config.gitSecret;
-const sigHeaderName = 'X-Hub-Signature-256'
+const sigHeaderName = 'X-Hub-Signature-256';
 let test;
 
 app.use(bodyParser.json());
@@ -15,35 +15,35 @@ app.use(bodyParser.json());
 
 app.post('/' ,function(request, response){
     const requestBody = request.body;
-    const requestHead = request.headers
+    const requestHead = request.headers;
 
     const payload = JSON.stringify(request.body);
 
     if (!payload){
-        console.log("Body is empty.")
+        console.log("Body is empty.");
         return;
     }
 
-    // calculate the signature
-    const expectedSignature = "sha256=" +
-        crypto.createHmac("sha256", secret)
-            .update(JSON.stringify(requestBody))
-            .digest("hex");
+    // // calculate the signature
+    // const expectedSignature = "sha256=" +
+    //     crypto.createHmac("sha256", secret)
+    //         .update(JSON.stringify(requestBody))
+    //         .digest("hex");
+    //
+    // //Request-Header Signature
+    // const signature = requestHead[sigHeaderName];
+    //
+    // // compare the signature against the one in the request
+    // if (signature !== expectedSignature) {
+    //     throw new Error("Invalid signature.");
+    // }
 
-    //Request-Header Signature
-    const signature = requestHead[sigHeaderName];
-
-    // compare the signature against the one in the request
-    if (signature !== expectedSignature) {
-        throw new Error("Invalid signature.");
-    }
-
-    console.log(payload)
-    console.log(request.body);
-    console.log(request.headers)
+    console.log(payload);
+    console.log(requestBody);
+    console.log(requestHead);
 
 });
 
 app.listen(7021);
-console.log("Listening on 7021...")
+console.log("Listening on 7021...");
 
