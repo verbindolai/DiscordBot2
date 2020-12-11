ad6bc580f488ac09400f862f4732d4aad11bea20
Verbindolai <nikolaiwieczorek@web.de>
2020-12-11 17:27:32 +0100

    Test

--- src/WebHookServer.ts
+++ src/WebHookServer.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-import crypto from "crypto";
 import fs from "fs";
 
 
@@ -11,33 +10,9 @@ const sigHeaderName = 'X-Hub-Signature-256';
 
 
 app.use(bodyParser.json());
-
-
 app.post('/' ,function(request, response){
     console.log(request.body)
-    console.log(request.headers)
-
-    const payload = JSON.stringify(request.body);
-
-    if (!payload){
-        console.log("Body is empty.");
-        return;
-    }
-
-    // // calculate the signature
-    // const expectedSignature = "sha256=" +
-    //     crypto.createHmac("sha256", secret)
-    //         .update(JSON.stringify(requestBody))
-    //         .digest("hex");
-    //
-    // //Request-Header Signature
-    // const signature = requestHead[sigHeaderName];
-    //
-    // // compare the signature against the one in the request
-    // if (signature !== expectedSignature) {
-    //     throw new Error("Invalid signature.");
-    // }
-
+    //console.log(request.headers)
 });
 
 app.listen(7021);
