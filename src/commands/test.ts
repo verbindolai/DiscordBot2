8a1ff7273c8c81d17b43df4066616aae97df29d2
Nikolai <69508529+verbindolai@users.noreply.github.com>
2021-03-04 10:52:09 +0100

    Update test.ts

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -11,7 +11,7 @@ class test implements commandInterface{
         this.ex = new executor(this.executeFunc, this.validateFunc)
     }
     private executeFunc(msg: Message, args: string[], client : Client): void {
-        msg.channel.send("Test erfolgreich....");
+        msg.channel.send("Test erfolgreich...");
     }
     validateFunc(msg : Message) : boolean{
         return true;
